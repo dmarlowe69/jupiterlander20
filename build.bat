81e349262c0507a88cd56457e4cbbbed23e8c49a
william marlowe <dmarlowe69@icloud.com>
2026-02-20 20:01:00 -0600

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,9 @@
+rem *.asm EOL CR
+c:\bin\cbm6502asm %1,%1=%1 /B1 /Z
+pause
+c:\bin\obj2prg %1.obj %1.prg
+pause
+
+
+
+
---
+++ jupiterlander20.bat
@@ -0,0 +1,10 @@
+call build jupiterlander20
+pause
+
+c:/bin/cartconv -t vic20 -i jupiterlander20.prg -o jupiterlander20.crt -n "JUPITER LANDER"
+pause
+
+
+
+
+
